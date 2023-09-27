5e74bd93fa63b4316a8ab6d54e89c537ffcc7eeb
shavvvv <shavaungreen102@gmail.com>
2023-09-27 16:18:05 -0400

    question 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/*
+
 console.log(34)
 
 // defining a function
@@ -35,3 +37,36 @@ function add(x, y){
 }
 
 console.log(add(4,5))
+*/
+
+///////////////////      Lab    ////////////
+
+
+/*
+(completed above) Define a function, as a function
+ declaration, maxOfTwoNumbers that takes two numbers 
+ as arguments and returns the largest of them. If they 
+ are the same, return that number. Use the if-else 
+ construct or a ternary expression - the Math.max method is not allowed.
+*/
+
+//   1. 
+
+
+// 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+    
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+  
+  console.log(maxOfTwoNumbers(3, 9));
+  
+  // 2.
+  
+  
