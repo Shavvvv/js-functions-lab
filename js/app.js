5edbcbc403ccf3fd9b69da73f081480b36db5237
shavvvv <shavaungreen102@gmail.com>
2023-09-27 15:31:24 -0400

    Greeting functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 console.log(34)
+
+// defining a function
+// declaring a function
+// creating the reusable block of code 
+// to be used at a later time in the program
+function sayHello (name){
+// write the code to say hello here
+console.log(`Hello ${name}`);
+
+}
+
+
+
+// execute
+sayHello('Shavon');
+
+
+function sayGoodBye(phrase,name){
+    console.log(`${phrase} ${name}`)
+}
+
+sayGoodBye("Adios", "laura");
+
+sayGoodBye("Chao", "Becky");
+
+sayGoodBye("GoodBye", "Chad");
+
+sayGoodBye("Peace", "Frankie");
